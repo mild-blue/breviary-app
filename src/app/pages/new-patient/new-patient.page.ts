a0190ee44f86ac28f04f739b24210989c2e05987
anastasia <anastasia.surikova@akka.eu>
2020-11-21 23:19:49 +0100

    toast on new patients

--- src/app/pages/new-patient/new-patient.page.ts
+++ src/app/pages/new-patient/new-patient.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Patient } from '../../model/Patient';
 import { ApiService } from '../../services/api/api.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new-patient',
@@ -13,6 +14,7 @@ export class NewPatientPage implements OnInit {
   public patient: Patient = new Patient();
 
   constructor(private apiService: ApiService,
+              private toastController: ToastController,
               private router: Router) {
   }
 
@@ -22,7 +24,21 @@ export class NewPatientPage implements OnInit {
   public save(patient: Patient): void {
     this.apiService.addPatient(patient).subscribe(p => {
       console.log('P from BE', p);
-      // this.router.navigate(['/home']);
+      this.presentToast('Patient added successfully').then(() => {
+        this.router.navigate(['/detail', p.id]);
+      });
     });
+  }
+
+  async presentToast(text: string): Promise<void> {
+    const toast = await this.toastController.create({
+      message: text,
+      duration: 2000,
+      position: 'top',
+      color: 'success',
+      cssClass: 'ion-text-center'
+    });
+
+    return toast.present();
   }
 }

f563337f6fd639e0d46d4a2f26758cb668dc0597
anastasia <anastasia.surikova@akka.eu>
2020-11-21 01:57:51 +0100

    removed outdated plugin

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+// import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { Platform } from '@ionic/angular';
 
 @Injectable({
@@ -7,30 +7,29 @@ import { Platform } from '@ionic/angular';
 })
 export class NotificationService {
 
-  constructor(private localNotifications: LocalNotifications,
-              private platform: Platform) {
+  constructor(private platform: Platform) {
   }
 
   public scheduleLocalNotification(showAfterHr: number): void {
     const isAndroid = this.platform.is('android');
 
     // Schedule delayed notification
-    this.localNotifications.schedule({
-      title: 'aPTT check for patient',
-      text: 'Don\'t forget to check aPTT level for the patient',
-      // trigger: { in: 15, unit: ELocalNotificationTriggerUnit.SECOND }, // todo: change to hours
-      sound: isAndroid ? 'file://sound.mp3' : 'file://beep.caf',
-      foreground: true,
-      sticky: true,
-      lockscreen: true,
-      priority: 2,
-      vibrate: true,
-      launch: true
-    });
-
-    this.localNotifications.on('click').subscribe(data => {
-      console.log(data);
-    });
+    // this.localNotifications.schedule({
+    //   title: 'aPTT check for patient',
+    //   text: 'Don\'t forget to check aPTT level for the patient',
+    //   // trigger: { in: 15, unit: ELocalNotificationTriggerUnit.SECOND }, // todo: change to hours
+    //   sound: isAndroid ? 'file://sound.mp3' : 'file://beep.caf',
+    //   foreground: true,
+    //   sticky: true,
+    //   lockscreen: true,
+    //   priority: 2,
+    //   vibrate: true,
+    //   launch: true
+    // });
+    //
+    // this.localNotifications.on('click').subscribe(data => {
+    //   console.log(data);
+    // });
   }
 
   // public showNotificcation()

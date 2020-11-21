ad4610e28e9f1e992f4009110d4e2848e6669173
Marek Polak <marek.polak@mild.blue>
2020-11-21 16:14:21 +0100

    compute timediff

--- src/app/pages/detail/detail.page.ts
+++ src/app/pages/detail/detail.page.ts
@@ -4,6 +4,12 @@ import { ApiService } from '@app/services/api/api.service';
 import { Patient, PatientHistoryEntry } from '@app/model/Patient';
 import { NotificationService } from '@app/services/notification/notification.service';
 
+interface TimeDiff {
+  hours: string
+  minutes: string
+  seconds: string
+}
+
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
@@ -49,19 +55,45 @@ export class DetailPage implements OnInit {
   private _initTimeUpdate(): void {
     setInterval(() => {
       if (this.nextReminderDate) {
-        // todo: luky
-        const start = new Date().getTime();
-        const end = this.nextReminderDate.getTime();
-
-        var diff = (start - end) / 1000;
-        diff /= 60;
-        const seconds = Math.abs(Math.round(diff));
-
-        console.log(seconds);
-        this.timeLeft = '';
+        const timeDiff = DetailPage._get_time_diff(this.nextReminderDate, new Date())
+        this.timeLeft = `${timeDiff.hours}:${timeDiff.minutes}:${timeDiff.seconds}`;
       }
     }, 1000);
   }
+
+  private static _formatNumber(num: number) {
+    return num < 10 ? `0${num}` : `${num}`
+  }
+
+  private static _get_time_diff(reminderDate: Date, now: Date): TimeDiff {
+    if (reminderDate.getTime() <= now.getTime()) {
+      return {
+        hours: '0',
+        minutes: '0',
+        seconds: '0',
+      }
+    }
+
+    let delta = Math.abs(reminderDate.getTime() - now.getTime()) / 1000;
+
+    const days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+
+    const hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+
+    const minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+
+    const seconds = Math.floor(delta % 60);
+
+    return {
+      hours: DetailPage._formatNumber(hours),
+      minutes: DetailPage._formatNumber(minutes),
+      seconds: DetailPage._formatNumber(seconds)
+    }
+  }
+
 
   public getPatientAge(): number {
     if (!this.patient) {
